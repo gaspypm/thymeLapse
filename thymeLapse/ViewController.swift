17fbc2ecd56ff422fce7cf7a6e34d82f79fd93ce
Gaspar Pérez Mazás <gaspypm@gmail.com>
2024-02-23 09:55:55 -0300

    Added actions to buttons

--- thymeLapse/ViewController.swift
+++ thymeLapse/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var startTimelapseButton: UIButton!
+    @IBOutlet weak var continueTimelapseButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func startTimelapseButtonPressed(_ sender: UIButton) {
+        
+        print("Start Timelapse button pressed")
+    }
+
+    @IBAction func continueTimelapseButtonPressed(_ sender: UIButton) {
+        print("Continue Timelapse button pressed")
     }
 
 }
